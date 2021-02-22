7ec255c921b3eeb40af497aff1e8bd73d374e7bc
Muhammad Hamza Asif <54440912+hamzaasyf@users.noreply.github.com>
2021-02-22 17:07:29 +0500

    Add files via upload
    Capturing rtsp camera using OpenCV

--- rtspCapture.py
+++ rtspCapture.py
@@ -27,6 +27,7 @@ while(cap.isOpened()):
 
     # Write the frame into the file 'output.avi'
     out.write(frame)
+    
     # # Press Q on keyboard to  exit
     # if cv2.waitKey(25) & 0xFF == ord('q'):
     #   break
@@ -35,38 +36,6 @@ while(cap.isOpened()):
   else: 
     break
 
-# When everything done, release the video capture object
-cap.release()
-
-# Closes all the frames
-# cv2.destroyAllWindows()
-
-# # Default resolutions of the frame are obtained.The default resolutions are system dependent.
-# # We convert the resolutions from float to integer.
-# frame_width = int(cap.get(3))
-# frame_height = int(cap.get(4))
-
-# # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-# out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-
-# while(True):
-#   ret, frame = cap.read()
-
-#   if ret == True: 
-    
-#     # Write the frame into the file 'output.avi'
-#     out.write(frame)
-
-#     # Display the resulting frame    
-#     cv2.imshow('frame',frame)
-
-#     # # Press Q on keyboard to stop recording
-#     # if cv2.waitKey(1) & 0xFF == ord('q'):
-#     #   break
-
-#   # Break the loop
-#   else:
-#     break  
 
 # # When everything done, release the video capture and video write objects
 cap.release()
